63dd3e89496f1da67ea28532d3b58d0563e03596
BentouDev <bentkowski.jakub@gmail.com>
2019-02-23 00:09:37 +0100

    Camera and Hero movement

---
+++ Assets/Data/Scripts/FollowingCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    public Transform Target;
+    public float HorizontalMargin = 100;
+    public float VerticalMargin = 100;
+    public float LerpSpeed = 3;
+
+    public float DampTime = 0.3f;
+    
+    private Vector3 ScreenPos;
+    private Vector3 Velocity;
+    
+    protected float GetX()
+    {
+        if (ScreenPos.x > HorizontalMargin && ScreenPos.x < Screen.width - HorizontalMargin)
+            return transform.position.x;
+        return Mathf.Lerp(transform.position.x, Target.position.x, Time.deltaTime * LerpSpeed);
+    }
+
+    protected float GetY()
+    {
+        if (ScreenPos.y > VerticalMargin && ScreenPos.y < Screen.height - VerticalMargin)
+            return transform.position.y;
+        return Mathf.Lerp(transform.position.y, Target.position.y, Time.deltaTime * LerpSpeed);
+    }
+
+    private void Update()
+    {
+        ScreenPos = Camera.main.WorldToScreenPoint(Target.position);
+        
+        var delta = Target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, ScreenPos.z));
+        var oldZ = transform.position.z;
+        var destination = transform.position + delta;
+        
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref Velocity, DampTime);
+    }
+}
---
+++ Assets/Data/Scripts/PlayerMovement.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Serialization;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool DrawDebug;
+
+    [Header("Physics")] public Rigidbody2D Body;
+    public float MaxSpeed;
+    public LayerMask GroundLayers;
+
+    private float CurrentVelocity;
+    private float AirVelocity;
+
+    [FormerlySerializedAs("JumpHeight")] 
+    public float JumpPower = 2;
+    public float MinJumpTime = 1;
+    public float MaxJumpTime = 1;
+
+    private float LastJumpTime;
+    private float LastJumpButtonTime;
+
+    public EdgeCollider2D GroundDetector;
+
+    public float MinJumpDistance = 0.2f;
+
+    public float Gravity = Physics.gravity.y;
+
+    [Header("Input")] public string Horizontal = "Horizontal";
+    public string Jump = "Jump";
+
+    private bool _isGrounded;
+    private bool _canJump;
+
+    public bool IsJumping
+    {
+        get { return Time.time - LastJumpTime < MinJumpTime; }
+    }
+
+    public float JumpSpeed
+    {
+        get { return JumpPower; }
+    }
+
+    public float GetSpeed()
+    {
+        return MaxSpeed;
+    }
+
+    void Start()
+    {
+        LastJumpTime = 0;
+        
+        if (!Body)
+            Body = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+
+        if (IsGrounded())
+            AirVelocity = 0;
+        else
+        {
+            if (IsJumping)
+            {
+                AirVelocity = JumpSpeed;
+            }
+            else
+            {
+                AirVelocity += Gravity;    
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _canJump = AirVelocity < 0 && Physics2D.Raycast(transform.position, Vector2.down, MinJumpDistance, GroundLayers);
+        _isGrounded = GroundDetector.IsTouchingLayers(GroundLayers);
+        Body.velocity = new Vector2(CurrentVelocity, AirVelocity) * Time.fixedDeltaTime;
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetButton(Jump))
+            DoJump();
+
+        DoInput(Input.GetAxis(Horizontal));
+    }
+
+    void DoInput(float directional)
+    {
+        CurrentVelocity = GetSpeed() * directional;
+        CurrentVelocity = Mathf.Clamp(CurrentVelocity,-MaxSpeed, MaxSpeed);
+    }
+
+    void DoJump()
+    {
+        if (IsJumping && Time.time - LastJumpButtonTime < MaxJumpTime)
+        {
+            LastJumpTime = Time.time;
+            AirVelocity = JumpSpeed;
+        }
+        else if (_canJump || IsGrounded())
+        {
+            _isGrounded = false;
+            
+            LastJumpTime = Time.time;
+            LastJumpButtonTime = Time.time;
+            AirVelocity = JumpSpeed;
+        }
+    }
+
+    void DoSkill()
+    {
+        
+    }
+
+    bool IsGrounded()
+    {
+        return _isGrounded;
+    }
+    
+    void OnGUI()
+    {
+        if (!DrawDebug)
+            return;
+        
+        GUI.Label(new Rect(10,10,200,30), "isGrounded: " + IsGrounded());
+        GUI.Label(new Rect(10,30,200,30), "canJump: " + (_canJump || IsGrounded()));
+        GUI.Label(new Rect(10,50,200,30), "air vel: " + AirVelocity);
+        GUI.Label(new Rect(10,70,200,30), "move vel: " + CurrentVelocity);
+    }
+}
