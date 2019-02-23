1aceebac9b8a4302f9a82015ebc1a43be583d614
BentouDev <bentkowski.jakub@gmail.com>
2019-02-23 19:26:31 +0100

    Internal prefabs in separate folder

--- Assets/Data/Scripts/KiwiBehaviour.cs
+++ Assets/Data/Scripts/KiwiBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class KiwiBehaviour : MonoBehaviour
 {
     public new Rigidbody2D rigidbody2D;
+    public Transform Model;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,11 @@ public class KiwiBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Model)
+            return;
+
+        var dir = rigidbody2D.velocity;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Model.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
