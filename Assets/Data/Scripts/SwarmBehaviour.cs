c6c2a2347c5ed6ea1b38c1cc3e3634bbad62aa11
rateforx <rateforx@gmail.com>
2019-02-23 01:43:45 +0100

    kiwi kiwi kiwi

---
+++ Assets/Data/Scripts/SwarmBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmBehaviour : MonoBehaviour
+{
+    public GameObject Kiwi;
+    public int kiwiCount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject kiwi = Instantiate( Kiwi );
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
