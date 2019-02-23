090f53e0a875592470afe09787a966e4568192b0
rateforx <rateforx@gmail.com>
2019-02-23 14:28:57 +0100

    swarmin

--- Assets/Data/Scripts/SwarmBehaviour.cs
+++ Assets/Data/Scripts/SwarmBehaviour.cs
@@ -6,38 +6,56 @@ public class SwarmBehaviour : MonoBehaviour
 {
     public GameObject Kiwi;
     public int kiwiCount = 50;
+    public Rigidbody2D swarmRigidbody2D;
     public List<GameObject> kiwis;
     public List<Rigidbody2D> rigidKiwis;
     public float swarmingSpeed = 100;
-    
+    public float spawnScatterRadius = 5;
+
     // Start is called before the first frame update
-    void Start()
+    void Start( )
     {
+//        swarmRigidbody2D = 
+        
         for ( int i = 0; i < kiwiCount; i++ )
         {
             GameObject kiwi = Instantiate( Kiwi, this.transform, true );
-            kiwi.transform.position = transform.position;
+            Vector2 vxy = Random.insideUnitCircle * spawnScatterRadius;
+            kiwi.transform.position = transform.position - new Vector3( vxy.x, vxy.y, 0 );
             kiwis.Add( kiwi );
-            rigidKiwis.Add( kiwi.GetComponent<Rigidbody2D>(  ) );
-            //todo place kiwi somewhere inside the shape
+            rigidKiwis.Add( kiwi.GetComponent<Rigidbody2D>( ) );
+            
         }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void FixedUpdate( )
     {
+        
+        
         for ( int i = 0; i < kiwis.Count; i++ )
         {
+            var kiwiPosition = kiwis[i].transform.position;
+            Vector2 directionToSwarmCenter = ( new Vector2( kiwiPosition.x, kiwiPosition.y ) -
+                                               new Vector2( transform.position.x, transform.position.y ) ).normalized;
+
             // forth quarter
-            if ( kiwis[ i ].transform.localPosition.x > 0 && kiwis[ i ].transform.localPosition.y < 0 )
+            if ( kiwis[i].transform.localPosition.x > 0 && kiwis[i].transform.localPosition.y < 0 )
             {
-                rigidKiwis[ i ].AddForce( new Vector2( -1, 0 ) * swarmingSpeed );
+                rigidKiwis[i].AddForce( directionToSwarmCenter + ( new Vector2( -1, 0 ) * swarmingSpeed ) );
             }
+
             // third quarter
-            if ( kiwis[ i ].transform.localPosition.x < 0 && kiwis[ i ].transform.localPosition.y < 0 )
+            if ( kiwis[i].transform.localPosition.x < 0 && kiwis[i].transform.localPosition.y < 0 )
             {
-                rigidKiwis[ i ].AddForce( new Vector2( 0, 1 ) * swarmingSpeed );
+                rigidKiwis[i].AddForce( directionToSwarmCenter + ( new Vector2( 0, 1 ) * swarmingSpeed ) );
             }
         }
+    }
+
+    private void OnDrawGizmos( )
+    {
+        Gizmos.DrawWireSphere( transform.position, spawnScatterRadius );
+        throw new System.NotImplementedException( );
     }
 }
